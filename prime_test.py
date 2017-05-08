2e287b6489b04c59c9699bc6606a300cf14d85c3
Akash <akashbaga011@gmail.com>
2017-05-09 01:54:25 +0300

    added new tests for floats, negatives and strings

--- prime_test.py
+++ prime_test.py
@@ -7,3 +7,9 @@ class TestPrimes(unittest.TestCase):
 		self.assertEqual(primes(5), [2,3,5])
 	def test_primes_2(self):
 		self.assertEqual(primes(20), [2,3,5,7,11,13,17,19])
+	def test_input_1(self):
+		self.assertEqual(primes(1), " Error: Prime numbers are greater than 1.")
+	def test_negative_input(self):
+		self.assertEqual(primes(-1), " Error: Prime numbers are greater than 1.")
+	def test_input_2(self):
+		self.assertEqual(primes("11"), " Error: Prime numbers are greater than 1.")
