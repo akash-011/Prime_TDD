37a7a60f2fedda0da9ad4363dfd814fc4eb2e308
Akash <akashbaga011@gmail.com>
2017-05-09 01:40:25 +0300

    added new test for primes

--- prime_test.py
+++ prime_test.py
@@ -5,3 +5,5 @@ class TestPrimes(unittest.TestCase):
 
 	def test_primes_1(self):
 		self.assertEqual(primes(5), [2,3,5])
+	def test_primes_2(self):
+		self.assertEqual(primes(20), [2,3,5,7,11,13,17,19])
