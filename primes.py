9c9105d8416a6f0d9a7d674a9bdc68e8b61c2452
Akash <akashbaga011@gmail.com>
2017-05-09 01:53:06 +0300

    Revised function after testing for negative and floats

--- primes.py
+++ primes.py
@@ -1,12 +1,15 @@
 def primes(n):
 	prime_list = []
-	for num in range(0,n + 1):
-	   # prime numbers are greater than 1
-	   if num > 1:
-	       for i in range(2,num):
-	           if (num % i) == 0:
-	               break
-	       else:
-			prime_list.append(num)
-	 
-	return sorted(prime_list)
+	if n > 1 and isinstance(n,int):
+		for num in range(0,n + 1):
+		   # prime numbers are greater than 1
+		   if num > 1:
+		       for i in range(2,num):
+		           if (num % i) == 0:
+		               break
+		       else:
+				prime_list.append(num)
+		 
+		return sorted(prime_list)
+	else:
+		return " Error: Prime numbers are greater than 1."
